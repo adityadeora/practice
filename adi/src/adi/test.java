bb903ef318d5f634aa1a409d958ab77fcd076a29
adityadeora <34486285+adityadeora@users.noreply.github.com>
2019-03-09 12:11:13 +0530

    Update test.java

--- adi/src/adi/test.java
+++ adi/src/adi/test.java
@@ -5,8 +5,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("adi");
-		System.out.println("deora");
-		System.out.println("adi");
 		System.out.println("sonali");
 	}
 
